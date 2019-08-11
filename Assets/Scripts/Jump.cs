2f0da061ca44a927477e8dcd5601080ec2545dd9
Cristiano Piemontese <cristiano.piemontese@gmail.com>
2019-08-11 14:12:46 +0200

    Added floor prefab + forward movement + jump

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public float constantVelocity = 1.0f;
+    public float impulseMagnitude = 1.0f;
+
+    private bool jumped = false;
+
+    // Start is called before the first frame update
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        jumped = Input.GetKeyDown(KeyCode.Space);
+    }
+
+    void FixedUpdate() {
+        Rigidbody2D rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D.velocity = new Vector2(constantVelocity, rigidbody2D.velocity.y);
+        if (jumped) {
+            rigidbody2D.AddForce(impulseMagnitude * Vector2.up, ForceMode2D.Impulse);
+        }
+    }
+}
