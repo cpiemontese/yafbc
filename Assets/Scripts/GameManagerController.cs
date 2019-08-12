71b77c727151e15b7f8cd760ddef2bc35ffded9e
Cristiano Piemontese <cristiano.piemontese@gmail.com>
2019-08-12 21:26:08 +0200

    Create world bounds trigger collider

---
+++ Assets/Scripts/GameManagerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerController : MonoBehaviour
+{
+    public Camera gameCamera;
+    public BoxCollider2D worldBounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 topRight = gameCamera.ViewportToWorldPoint(new Vector3(1, 1, gameCamera.nearClipPlane));
+        Vector3 topLeft = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, gameCamera.nearClipPlane));
+        Vector3 bottomRight = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, gameCamera.nearClipPlane));
+        Vector3 bottomLeft = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, gameCamera.nearClipPlane));
+
+        Debug.Log(topRight);
+        Debug.Log(topLeft);
+        Debug.Log(bottomRight);
+        Debug.Log(bottomLeft);
+        worldBounds.size = new Vector2(bottomRight.x - bottomLeft.x, topRight.y - bottomRight.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
