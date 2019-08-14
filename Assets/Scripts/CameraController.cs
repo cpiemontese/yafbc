1455d54c1898349fc6678c518935a9fbb9ca057b
Cristiano Piemontese <cristiano.piemontese@gmail.com>
2019-08-14 23:25:45 +0200

    Camera moves tiles around still player

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,15 +1,22 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public Transform lookAt;
+    public float tileMovementInterval = 1.0f;
     public GameObject topTile;
     public GameObject bottomTile;
 
-    ArrayList bottomTiles;
-    ArrayList topTiles;
+    float timeSinceLastTileMovement = 0.0f;
+
+    List<GameObject> topTiles;
+    List<GameObject> bottomTiles;
+    List<Tuple<Rigidbody2D, Rigidbody2D>> zippedTiles;
+
+    Vector3 topRight;
+    Vector3 bottomRight;
+    Vector3 bottomLeft;
 
     enum Coordinate {
         TopRight, TopLeft, BottomRight, BottomLeft   
@@ -20,53 +27,50 @@ public class CameraController : MonoBehaviour
     {
         var camera = GetComponent<Camera>();
         var collider2d = GetComponent<BoxCollider2D>();
-        bottomTiles = new ArrayList();
-        topTiles = new ArrayList();
+        bottomTiles = new List<GameObject>();
+        topTiles = new List<GameObject>();
+        zippedTiles = new List<Tuple<Rigidbody2D, Rigidbody2D>>();
 
-        Vector3 topRight = camera.ViewportToWorldPoint(new Vector3(1, 1, camera.nearClipPlane));
-        Vector3 bottomRight = camera.ViewportToWorldPoint(new Vector3(1, 0, camera.nearClipPlane));
-        Vector3 bottomLeft = camera.ViewportToWorldPoint(new Vector3(0, 0, camera.nearClipPlane));
+        topRight = camera.ViewportToWorldPoint(new Vector3(1, 1, camera.nearClipPlane));
+        bottomRight = camera.ViewportToWorldPoint(new Vector3(1, 0, camera.nearClipPlane));
+        bottomLeft = camera.ViewportToWorldPoint(new Vector3(0, 0, camera.nearClipPlane));
 
         collider2d.size = new Vector2(bottomRight.x - bottomLeft.x, topRight.y - bottomRight.y);
 
         var topY = topRight.y - 0.5f;
         var bottomY = bottomRight.y + 0.5f;
 
-        for (float d = bottomLeft.x + 0.5f; d < bottomRight.x + 2.5f; d++) {
-            topTiles.Add(Instantiate(topTile, new Vector3(d, topY), Quaternion.identity));
-            bottomTiles.Add(Instantiate(bottomTile, new Vector3(d, bottomY), Quaternion.identity));
+        for (float d = bottomLeft.x + 0.5f; d < -12f; d++) {
+            var newTopTile = Instantiate(topTile, new Vector3(d, topY), Quaternion.identity);
+            var newBottomTile = Instantiate(bottomTile, new Vector3(d, bottomY), Quaternion.identity);
+            topTiles.Add(newTopTile);
+            bottomTiles.Add(newBottomTile);
+            zippedTiles.Add(Tuple.Create(
+                newTopTile.GetComponent<Rigidbody2D>(),
+                newBottomTile.GetComponent<Rigidbody2D>()
+            ));
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.SetPositionAndRotation(
-            new Vector3(lookAt.position.x, transform.position.y, transform.position.z),
-            transform.rotation
-        );
     }
 
-    Vector3 GetCoordinate(Coordinate coordinate) {
-        var camera = GetComponent<Camera>();
-
-        switch (coordinate)
-        {
-            case Coordinate.TopRight:
-                return camera.ViewportToWorldPoint(new Vector3(1, 1, camera.nearClipPlane));
-            case Coordinate.BottomRight:
-                return camera.ViewportToWorldPoint(new Vector3(1, 0, camera.nearClipPlane));
-            case Coordinate.BottomLeft:
-                return camera.ViewportToWorldPoint(new Vector3(0, 0, camera.nearClipPlane));
-            case Coordinate.TopLeft:
-                return camera.ViewportToWorldPoint(new Vector3(0, 1, camera.nearClipPlane));
+    void FixedUpdate() {
+        timeSinceLastTileMovement += Time.fixedDeltaTime;
+        if (timeSinceLastTileMovement >= tileMovementInterval) {
+            timeSinceLastTileMovement -= tileMovementInterval;
+            zippedTiles.ForEach(tuple => {
+                tuple.Item1.MovePosition(new Vector2(tuple.Item1.position.x - 0.5f, tuple.Item1.position.y));
+                tuple.Item2.MovePosition(new Vector2(tuple.Item2.position.x - 0.5f, tuple.Item2.position.y));
+            });
         }
-        return Vector3.zero;
     }
 
     void OnTriggerExit2D(Collider2D other) {
         var oldY = other.gameObject.GetComponent<Rigidbody2D>().position.y;
-        var newX = GetCoordinate(Coordinate.TopRight).x + 1.5f;
+        var newX = topRight.x + 1.5f;
         other.gameObject.transform.SetPositionAndRotation(new Vector3(newX, oldY), Quaternion.identity);
         //other.gameObject.GetComponent<Rigidbody2D>().MovePosition(new Vector2(newX, oldY));
     }
--- Assets/Scripts/TileController.cs
+++ Assets/Scripts/TileController.cs
@@ -7,13 +7,11 @@ public class TileController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     void OnCollisionEnter2D(Collision2D other) {
