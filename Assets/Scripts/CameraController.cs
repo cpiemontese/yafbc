267b74c8d44b4f37c57dcf1c816f0b2e2765be3c
Cristiano Piemontese <cristiano.piemontese@gmail.com>
2019-08-21 22:00:58 +0200

    Obstacle update
    - obstacles get generated every fixed interval with some probability
    - obstacles move
    - obstacle get destroyed when not on camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public float spawnTimer = 1.0f;
+    public float spawnProbability = 0.5f;
     public float tileMovementSpeed = 1.0f;
     public GameObject topTile;
     public GameObject bottomTile;
     public ObstacleGeneratorController obstacleGeneratorController;
 
-    List<GameObject> topTiles;
-    List<GameObject> bottomTiles;
+    Queue<GameObject> obstacles;
     List<Tuple<Rigidbody2D, Rigidbody2D>> zippedTiles;
 
+    float cameraHeight;
+    float timeSinceLastSpawn;
     Vector3 topRight;
     Vector3 bottomRight;
     Vector3 bottomLeft;
@@ -26,8 +29,9 @@ public class CameraController : MonoBehaviour
     {
         var camera = GetComponent<Camera>();
         var collider2d = GetComponent<BoxCollider2D>();
-        bottomTiles = new List<GameObject>();
-        topTiles = new List<GameObject>();
+
+        timeSinceLastSpawn = 1.0f;
+        obstacles = new Queue<GameObject>();
         zippedTiles = new List<Tuple<Rigidbody2D, Rigidbody2D>>();
 
         topRight = camera.ViewportToWorldPoint(new Vector3(1, 1, camera.nearClipPlane));
@@ -41,37 +45,60 @@ public class CameraController : MonoBehaviour
         var topTilesContainer = new GameObject("Top tiles container");
         var bottomTilesContainer = new GameObject("Bottom tiles container");
 
-        for (float d = bottomLeft.x + 0.5f; d < bottomRight.x + 2.5f; d++) {
+        for (float d = bottomLeft.x + 0.5f; d < bottomRight.x + 2.5f; d++)
+        {
             var newTopTile = Instantiate(topTile, new Vector3(d, topY), Quaternion.identity, topTilesContainer.transform);
             var newBottomTile = Instantiate(bottomTile, new Vector3(d, bottomY), Quaternion.identity, bottomTilesContainer.transform);
-            topTiles.Add(newTopTile);
-            bottomTiles.Add(newBottomTile);
             zippedTiles.Add(Tuple.Create(
                 newTopTile.GetComponent<Rigidbody2D>(),
                 newBottomTile.GetComponent<Rigidbody2D>()
             ));
         }
 
-        var height = topRight.y - bottomRight.y - 3f;
-        var obstacle = obstacleGeneratorController.GenerateObstacle(Mathf.RoundToInt(height));
-        obstacle.transform.SetPositionAndRotation(new Vector3(bottomRight.x, bottomRight.y + 1.5f, 0), Quaternion.identity);
+        cameraHeight = topRight.y - bottomRight.y - 3f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (timeSinceLastSpawn >= spawnTimer && UnityEngine.Random.Range(0f, 1f) < spawnProbability)
+        {
+            var obstacle = obstacleGeneratorController.GenerateObstacle(Mathf.RoundToInt(cameraHeight));
+            obstacle.transform.SetPositionAndRotation(new Vector3(bottomRight.x + 1.5f, bottomRight.y + 1.5f, 0), Quaternion.identity);
+            obstacles.Enqueue(obstacle);
+            timeSinceLastSpawn -= 1.0f;
+        }
+        timeSinceLastSpawn += Time.deltaTime;
     }
 
-    void FixedUpdate() {
+    void FixedUpdate()
+    {
+        var movementMagnitude = tileMovementSpeed * Time.fixedDeltaTime;
         zippedTiles.ForEach(tuple => {
-            tuple.Item1.MovePosition(new Vector2(tuple.Item1.position.x - tileMovementSpeed * Time.fixedDeltaTime, tuple.Item1.position.y));
-            tuple.Item2.MovePosition(new Vector2(tuple.Item2.position.x - tileMovementSpeed * Time.fixedDeltaTime, tuple.Item2.position.y));
+            tuple.Item1.MovePosition(new Vector2(tuple.Item1.position.x - movementMagnitude, tuple.Item1.position.y));
+            tuple.Item2.MovePosition(new Vector2(tuple.Item2.position.x - movementMagnitude, tuple.Item2.position.y));
         });
+
+        foreach (var obstacle in obstacles)
+        {
+            obstacle.GetComponent<Rigidbody2D>().MovePosition(new Vector2(
+                obstacle.transform.position.x - movementMagnitude,
+                obstacle.transform.position.y
+            ));
+        }
     }
 
-    void OnTriggerExit2D(Collider2D other) {
-        var oldY = other.gameObject.GetComponent<Rigidbody2D>().position.y;
-        var newX = topRight.x + 1.5f;
-        other.gameObject.transform.SetPositionAndRotation(new Vector3(newX, oldY), Quaternion.identity);
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Obstacle")
+        {
+            Destroy(other.gameObject);
+            obstacles.Dequeue();
+        } else
+        {
+            var oldY = other.gameObject.GetComponent<Rigidbody2D>().position.y;
+            var newX = topRight.x + 1.5f;
+            other.gameObject.transform.SetPositionAndRotation(new Vector3(newX, oldY), Quaternion.identity);
+        }
     }
 }
--- Assets/Scripts/ObstacleGeneratorController.cs
+++ Assets/Scripts/ObstacleGeneratorController.cs
@@ -20,7 +20,19 @@ public class ObstacleGeneratorController : MonoBehaviour
 
     public GameObject GenerateObstacle(int maxHeight) {
         int height = Random.Range(1, maxHeight);
+
         GameObject obstacle = new GameObject("Obstacle");
+        obstacle.tag = "Obstacle";
+        obstacle.AddComponent<BoxCollider2D>();
+        var bc2d = obstacle.GetComponent<BoxCollider2D>();
+        bc2d.size = new Vector2(1f, 8f);
+        bc2d.offset = new Vector2(0f, 3.5f);
+        bc2d.isTrigger = true;
+        obstacle.AddComponent<Rigidbody2D>();
+        var rb2d = obstacle.GetComponent<Rigidbody2D>();
+        rb2d.isKinematic = true;
+        rb2d.constraints = RigidbodyConstraints2D.FreezePositionY;
+
         for (int i = 0; i < height/2; i++) {
             Instantiate(obstacleTile, new Vector3(0, i, 0), Quaternion.identity, obstacle.transform);
         }
